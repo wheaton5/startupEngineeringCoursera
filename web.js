9557ef2e13b51ed51a051601978cfea5c5a8f1f3
wheaton5 <whheaton@gmail.com>
2013-07-14 17:30:12 +0000

    changes to web.js to read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,16 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World???!');
+  var fs = require('fs');
+  var json = '';
+  var str = fs.readFileSync('index.html', function(err,data){
+	if(err) throw err;
+	
+  json = JSON.stringify(data);
+});
+
+  
+  response.send(json);
 });
 
 var port = process.env.PORT || 5000;
